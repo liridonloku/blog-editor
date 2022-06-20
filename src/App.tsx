a6ebaba3224a02fb71d7099e90a4cef04fbc0085
Liridon Loku <liridonloku@gmail.com>
2022-06-20 13:50:17 +0200

    Install bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
-function App() {
-  return <div className="App">A</div>;
-}
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
